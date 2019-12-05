eb8b1d3a69bdb6588cd84e7015dc5f659d153717
Andrew MacLean <anma6580@colorado.edu>
2019-12-05 16:56:12 -0700

    starting hash

---
+++ hash.cpp
@@ -0,0 +1,81 @@
+
+#include<iostream>
+#include "hash.hpp"
+
+
+using namespace std;
+
+node* HashTable::createNode(int key, node* next)
+{
+    node* nw = new node;
+    nw->key = key;
+    nw->next = next;
+    return nw;
+}
+
+HashTable::HashTable(int bsize)
+{
+    this->tableSize= bsize;
+    table = new node*[tableSize];
+    for(int i=0;i<bsize;i++)
+        table[i] = nullptr;
+}
+
+
+unsigned int HashTable::hashFunction(int key)
+{
+    return (key % tableSize);
+}
+
+
+//function to search
+node* HashTable::searchItem(int key)
+{
+    //Compute the index by using the hash function
+    int index = hashFunction(key);
+
+    
+    
+    for (int i = 0; i < tableSize; i++) {
+        if (table[key] == index) {
+            cout << "Found" << endl;
+            return NULL;
+        }
+    }
+    cout << "Not Found" << endl;
+}
+
+
+//function to insert
+bool HashTable::insertItem(int key)
+{
+    if(!searchItem(key))
+    {
+        
+        int index = hashFunction(key);
+       node* nw= createNode(key,nullptr);
+        
+        if (table[index]==NULL) {
+            table[index] = nw;
+        }
+        else
+            break;
+     }
+    else{
+        cout<<"duplicate entry: "<<key<<endl;
+        return false;
+    }
+
+}
+
+
+void HashTable::printTable()
+{
+    for (int i = 0; i < tableSize; i++) {
+        cout << i <<"|| ";
+
+        
+        
+    }
+
+ }
